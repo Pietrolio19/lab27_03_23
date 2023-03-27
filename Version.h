127a4ff4ce260b8310e5cd7a603e8492c3e4c5c5
Pietrolio19 <pietromadioni224@gmail.com>
2023-03-27 17:28:27 +0200

    Aggiunta di Header Version

---
+++ Version.h
@@ -0,0 +1,12 @@
+//
+// Created by 167674 on 27/03/2023.
+//
+
+#ifndef GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+#define GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+
+#include <string>
+
+static const std::string VERSION = "1.0";
+
+#endif //GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
